05f8a1fb2fb068b5e87d02ba27bb0934672bacf3
Kem98m <46077117+Kem98m@users.noreply.github.com>
2023-01-07 11:39:03 -0500

    Add files via upload

---
+++ p0-person/src/ManyPeopleDriver.java
@@ -0,0 +1,18 @@
+/**
+ * @author Kenneth Marenco
+ * @date September 4, 2019
+ */
+public class ManyPeopleDriver {
+    public static void main(String[] args) {
+        String[] names = {
+                "Sam", "Chris", "Zoe", "Sandy", "Terry", "Alex", "Jesse", "Riley"
+        };
+        int[] ages = {
+                5,10,15,20,25,30,35,40
+        };
+        for(int k=0; k < names.length; k++) {
+            Person p = new Person(names[k], ages[k]);
+            System.out.println(p);
+        }
+    }
+}
---
+++ p0-person/src/PersonDriver.java
@@ -0,0 +1,15 @@
+/**
+@author Kenneth Marenco
+ @date September 4, 2019
+ */
+public class PersonDriver {
+	public static void main(String[] args) {
+		Person p = new Person();
+		Person q = new Person("Sam",21);
+		Person r = new Person("Chris", 99);
+
+		System.out.println(p);
+		System.out.println(q);
+		System.out.println(r);
+	}
+}
